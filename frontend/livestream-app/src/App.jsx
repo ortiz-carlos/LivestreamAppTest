afdbb0faaf27c41c6323d5286e7b51abe92d4aae
Carlos Ortiz <slouzzy@gmail.com>
2025-04-03 15:07:10 -0600

    user login system 90% done

--- frontend/livestream-app/src/App.jsx
+++ frontend/livestream-app/src/App.jsx
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import StreamPage from './pages/StreamPage';
 import AdminPage from './pages/AdminPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/stream" element={<StreamPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
